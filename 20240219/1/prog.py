1d4d407659646f3896d7bca1dc034678d030f4ee
Stephen <UsamUNX>
2024-02-24 11:54:36 +0300

    work with two arguments: printing commit-object info

--- 20240219/1/prog.py
+++ 20240219/1/prog.py
@@ -8,3 +8,15 @@ if len(sys.argv) == 2:
         with os.scandir(heads) as dr:
             for item in dr:
                 print(item.name)
+elif len(sys.argv) == 3:
+    with open(sys.argv[1] + '/.git/refs/heads/' + sys.argv[2], 'r') as branch:
+        last_commit = branch.read()[:-1]
+    
+    with open(sys.argv[1] + '/.git/objects/' + 
+              last_commit[0:2] + '/' + last_commit[2:], 'rb') as commit:
+        commit_data = zlib.decompress(commit.read()).partition(b'\00')[2].split(b'\n')
+    print(commit_data[0].decode())
+    print(commit_data[1].decode())
+    print(*commit_data[2].decode().split(' ')[:-2])
+    print(*commit_data[3].decode().split(' ')[:-2])
+    
