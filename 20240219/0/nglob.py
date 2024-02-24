e8309a296147e40ada6b5e32c3fa50697685e0c5
Stephen <UsamUNX>
2024-02-24 10:56:33 +0300

    upd readability

--- 20240219/0/nglob.py
+++ 20240219/0/nglob.py
@@ -4,5 +4,5 @@ import sys
 
 for item in glob.glob(sys.argv[1] + "/??/*"):
     with open(item, "rb") as f:
-        print(zlib.decompress(f.read()))
+        print(zlib.decompress(f.read()), '\n\n')
         
