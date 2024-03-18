f3ce6a8e502d5a02a672060bbc674af0a600eaee
Stephen <UsamUNX>
2024-03-18 14:25:15 +0300

    Sem 20240318 pt2

--- 20240318/0/cli.py
+++ 20240318/0/cli.py
@@ -1,10 +1,39 @@
 import sys
 import socket
+import cmd
+
+
+class clicmd(cmd.Cmd):
+
+    def __init__(self, socket):
+        self.s = socket
+        return super().__init__()
+
+    prompt = ">> "
+
+    def do_EOF(self, args):
+        return 1
+
+    def emptyline(self):
+        pass
+
+    def do_print(self, args):
+        self.s.sendall(("print " + args).encode())
+        print(self.s.recv(1024).rstrip().decode())
+
+    def do_info(self, args):
+        self.s.sendall(("info " + args).encode())
+        print(self.s.recv(1024).rstrip().decode())
+
+    def complete_info(self, text, line, begidx, endidx):
+        words = (line[:endidx] + '.').split()
+
+        if len(words) == 2:
+            return [c for c in ["host", "port"] if c.startswith(text)]
+    
 
 host = "localhost" if len(sys.argv) < 2 else sys.argv[1]
 port = 1337 if len(sys.argv) < 3 else int(sys.argv[2])
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((host, port))
-    while msg := sys.stdin.buffer.readline():
-        s.sendall(msg)
-        print(s.recv(1024).rstrip().decode())
+    clicmd(s).cmdloop()
--- 20240318/0/srv.py
+++ 20240318/0/srv.py
@@ -1,12 +1,21 @@
 import socket
 import sys
 import multiprocessing
+import shlex
 
 def serve(conn, addr):
     with conn:
       print('Connected by', addr)
       while data := conn.recv(1024):
-            conn.sendall(data.swapcase())
+            data1 = shlex.split(data.decode())
+
+            match data1:
+                case ['print', words]:
+                    conn.sendall(words.encode())
+                case ['info', 'host'|'port']:
+                    printer = addr[0] if data1[1] == 'host' else addr[1]
+                    conn.sendall(str(printer).encode())
+
 
 host = "localhost" if len(sys.argv) < 2 else sys.argv[1]
 port = 1337 if len(sys.argv) < 3 else int(sys.argv[2])
