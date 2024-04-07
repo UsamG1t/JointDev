ee65fd5db5308806a6a788bcb46dd1f5f4209f47
Stephen <UsamUNX>
2024-04-07 10:29:39 +0300

    Base for HW 20240401 1

---
+++ 20240401/1/client.py
@@ -0,0 +1,223 @@
+import cmd
+import shlex
+
+import cowsay
+import io
+
+import readline
+import threading
+import sys
+import socket
+
+def msg_sendreciever(client, socket):
+    while response := socket.recv(1024).rstrip().decode():
+        print(f"\n{response}\n{client.prompt}{readline.get_line_buffer()}", end="", flush=True)
+
+
+weapons = {
+'sword': 10,
+'spear': 15,
+'axe'  : 20
+}
+
+custom_monster = cowsay.read_dot_cow(io.StringIO("""
+$the_cow = <<EOC;
+         $thoughts
+          $thoughts
+    ,_                    _,
+    ) '-._  ,_    _,  _.-' (
+    )  _.-'.|\\--//|.'-._  (
+     )'   .'\/o\/o\/'.   `(
+      ) .' . \====/ . '. (
+       )  / <<    >> \  (
+        '-._/``  ``\_.-'
+  jgs     __\\'--'//__
+         (((""`  `"")))
+EOC
+"""))
+
+class  MUDcmd(cmd.Cmd):
+
+    def __init__(self, socket):
+        self.field_size = 10
+        self.socket = socket
+        print("<<< Welcome to Python-MUD 0.1 >>>")
+        return super().__init__()
+
+    prompt = ">> "
+
+    def do_EOF(self, args):
+        'Stops game by ^D combination'
+        return 1
+    def emptyline(self):
+        'auto-repeat of last command OFF'
+        return
+    
+    def do_up(self, args):
+        'one step UP on field'
+        
+        self.socket.sendall(f'move up\n'.encode())
+        # response = self.socket.recv(1024).rstrip().decode()
+        # response = shlex.split(response)
+        # move_answer(*response)
+    
+    def do_down(self, args):
+        'one step DOWN on field'
+        
+        self.socket.sendall(f'move down\n'.encode())
+        # response = self.socket.recv(1024).rstrip().decode()
+        # response = shlex.split(response)
+        # move_answer(*response)
+    
+    def do_left(self, args):
+        'one step LEFT on field'
+        
+        self.socket.sendall(f'move left\n'.encode())
+        # response = self.socket.recv(1024).rstrip().decode()
+        # response = shlex.split(response)
+        # move_answer(*response)
+    
+    def do_right(self, args):
+        'one step RIGHT on field'
+        
+        self.socket.sendall(f'move right\n'.encode())
+        # response = self.socket.recv(1024).rstrip().decode()
+        # response = shlex.split(response)
+        # move_answer(*response)
+    
+    def do_addmon(self, args):
+        '''
+        Add monster on the position
+
+        first argument is a name of monster from [cowsay.list_cows() | 'jgsbat']
+
+        Other required args (their order is not important):
+        
+        1. coord <int[0...field_size)> <int[0...field_size)>
+        2. hp <int[0...inf)>
+        3. hello <string (with quotation for more than one word)>
+        '''
+        
+        args = shlex.split(args)
+        broken = False
+
+        if len(args) != 8:
+            broken = True
+            print('client: Invalid arguments (count of elements)')
+
+        elif not isinstance(args[0], str):
+            broken = True
+            print('client: Invalid arguments (type of name)')
+
+        elif args[0] not in cowsay.list_cows() \
+                and args[0] != 'jgsbat':
+            broken = True
+            print('client: Cannot add unknown monster')
+
+        else:
+            monster = {"name": args[0]}
+
+            for i in range(1, len(args)):
+                match args[i]:
+                    case 'hello':
+                        if not isinstance(args[i+1], str):
+                            broken = True
+                            print('client: Invalid arguments (type of message)')
+                            break
+                        monster["message"] = args[i+1]
+                    case 'hp':
+                        if not args[i+1].isdigit():
+                            broken = True
+                            print('client: Invalid arguments (type of hp)')
+                            break
+                        if int(args[i+1]) <= 0:
+                            broken = True
+                            print('client: Invalid arguments (value of hp)')
+                            break
+                        monster["hp"] = int(args[i+1])
+                    case 'coord':
+                        if not args[i+1].isdigit():
+                            broken = True
+                            print('client: Invalid arguments (type of coord x')
+                            break
+                        if int(args[i+1]) < 0 \
+                                or int(args[i+1]) >= self.field_size:
+                            broken = True
+                            print('client: Invalid arguments (value of coord x)')
+                            break
+                        if not args[i+2].isdigit():
+                            broken = True
+                            print('client: Invalid arguments (type of coord y')
+                            break
+                        if int(args[i+2]) < 0 \
+                                or int(args[i+2]) >= self.field_size:
+                            broken = True
+                            print('client: Invalid arguments (value of coord y)')
+                            break
+                        m_x = int(args[i+1])
+                        m_y = int(args[i+2])
+                    case _: continue
+        if not broken:
+            self.socket.sendall(f'addmon {monster["name"]} hp {monster["hp"]} coord {m_x} {m_y} hello "{monster["message"]}"\n'.encode())
+            # response = self.socket.recv(1024).rstrip().decode()
+            # response = shlex.split(response)
+            # addmon_answer(*response)
+
+
+    def do_attack(self, args):
+        'Attack the monster in current position'
+
+        weapon = None
+        args = shlex.split(args)
+        
+        match args:
+            case [name, 'with', weapon_type]:
+                if weapon_type not in weapons.keys():
+                    print('client: Unknown weapon')
+                else:
+                    weapon = {'type'  : weapon_type,
+                              'damage': weapons[weapon_type]}
+            case [name]:
+                weapon = {'type'  : 'sword',
+                          'damage': weapons['sword']}
+            case _:
+                print('client: Invalid command')
+
+        if weapon:
+            self.socket.sendall(f'attack {args[0]} {weapon["damage"]}\n'.encode())
+            # response = self.socket.recv(1024).rstrip().decode()
+            # response = shlex.split(response)
+            # attack_answer(args[0], *response)
+
+
+    def complete_attack(self, text, line, begidx, endidx):
+        words = (line[:endidx] + ".").split()
+        DICT = []
+        match len(words):
+            case 2:
+                DICT = cowsay.list_cows() + ['jgsbat']
+            case 4:
+                if words[2].startswith('with'):
+                    DICT = weapons.keys()
+        return [c for c in DICT if c.startswith(text)]
+
+
+
+
+if __name__ == '__main__':
+    # host = "localhost" if len(sys.argv) < 2 else sys.argv[1]
+    # port = 1337 if len(sys.argv) < 3 else int(sys.argv[2])
+    host = "localhost"
+    port = 1337
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        s.sendall(f'register {sys.argv[1]}\n'.encode())
+        response = s.recv(1024).rstrip().decode()
+        if response[0] == '0':
+            print(response[2:])
+            cli = MUDcmd(s)
+            request = threading.Thread(target = msg_sendreciever, args = (cli, cli.socket))
+            request.start()
+            cli.cmdloop()
+        else:
+            print(response)
---
+++ 20240401/1/server.py
@@ -0,0 +1,348 @@
+import cowsay
+import shlex
+import cmd
+import asyncio
+
+# import socket
+# import sys
+# import multiprocessing
+
+
+addmon_errors = {
+    '1' : 'Invalid arguments (count of elements)',
+    '2' : 'Invalid arguments (type of name)', 
+    '3' : 'Cannot add unknown monster', 
+    '4' : 'Invalid arguments (type of message)', 
+    '5' : 'Invalid arguments (type of hp)', 
+    '6' : 'Invalid arguments (value of hp)', 
+    '7' : 'Invalid arguments (type of coord x', 
+    '8' : 'Invalid arguments (value of coord x)', 
+    '9' : 'Invalid arguments (type of coord y', 
+    '10': 'Invalid arguments (value of coord y)'
+}
+
+def move_answer(x, y, name=None, message=None):
+    response = []
+    response.append(f'Moved to ({x}, {y})')
+
+    if name:
+        if name == 'jgsbat':
+            response.append(cowsay.cowsay(message, cowfile=custom_monster))
+        else:
+            response.append(cowsay.cowsay(message, cow=name))
+
+    return '\n'.join(response)
+
+
+def addmon_answer(code, name = None, hp = None, x = None, y = None, msg = None, replace_check=None):
+    response = []
+
+    if code != '0':
+        response.append(addmon_errors[code])
+        return '\n'.join(response)
+
+    # response.append(f'Added monster {name} with {hp} hp to ({x}, {y}) saying {msg}')
+    response.append(f'Added monster {name} with {hp} hp')
+    if replace_check:
+        response.append(replace_check)
+
+    return '\n'.join(response)
+
+def attack_answer(name, code, dmg = None, hp = None):
+    response = []
+    
+    if code == '1':
+        response.append(f'No {name} here')
+        return '\n'.join(response).encode()
+
+    response.append(f'Attacked {name}, damage {dmg} hp')
+
+    if hp != '0':
+        response.append(f'{name} now has {hp}')
+    else:
+        response.append(f'{name} died')
+
+    return '\n'.join(response)
+
+
+
+class Player:
+    steps = {
+    'up'   : {'x': 0, 'y':-1},
+    'down' : {'x': 0, 'y': 1},
+    'left' : {'x':-1, 'y': 0},
+    'right': {'x': 1, 'y': 0}
+    }
+
+    weapons = {
+    'sword': 10,
+    'spear': 15,
+    'axe'  : 20
+    }
+
+    def __init__(self):
+        self.x = self.y = 0
+        self.field_size = 10
+
+    def position(self):
+        return (self.x, self.y)
+
+    def move(self, method, args):
+        self.x = (self.x + self.field_size
+                 + self.steps[method]['x']) % self.field_size
+        self.y = (self.y + self.field_size
+                 + self.steps[method]['y']) % self.field_size
+
+        return self.position()
+
+class Monster:
+    def __init__(self, name, hp, message):
+        self.name = name
+        self.hp = hp
+        self.message = message
+    
+    def encounter(self):
+        return (self.name, self.message)
+
+    def damage(self, damage):
+        dmg = min([self.hp, damage])
+        self.hp = self.hp - dmg
+
+        return (self.name, dmg, self.hp)
+
+class Game:
+
+    def __init__(self):
+        self.field_size = 10
+        self.monsters = {} # key(x, y) -> Monster()
+        
+    def key(self, position):
+        return position[1] * self.field_size + position[0]
+    
+    def move(self, player, method, args):
+        response = []
+        print("LOG: ", 'move', method, args)
+
+        position = player.move(method, args)
+
+        response.append(str(position[0]))
+        response.append(str(position[1]))
+
+        key = self.key(position)
+        if self.monsters.setdefault(key, None) != None:
+            result = self.monsters[key].encounter()
+            response.append(result[0])
+            response.append(result[1])
+
+        print("LOG: response", response)
+        return response
+
+    def addmon(self, args):
+        response = []
+        broken = False
+        monster = {}
+        args = shlex.split(args)
+
+        print("LOG: ", 'addmon', args)
+
+        if len(args) != 8:
+            broken = True
+            response.append('1')
+
+        elif not isinstance(args[0], str):
+            broken = True
+            response.append('2')
+
+        elif args[0] not in cowsay.list_cows() \
+                and args[0] != 'jgsbat':
+            broken = True
+            response.append('3')
+        else:
+            monster = {"name": args[0]}
+
+            for i in range(1, len(args)):
+                match args[i]:
+                    case 'hello':
+                        if not isinstance(args[i+1], str):
+                            broken = True
+                            response.append('4')
+                            break
+                        monster["message"] = args[i+1]
+                    case 'hp':
+                        if not args[i+1].isdigit():
+                            broken = True
+                            response.append('5')
+                            break
+                        if int(args[i+1]) <= 0:
+                            broken = True
+                            response.append('6')
+                            break
+                        monster["hp"] = int(args[i+1])
+                    case 'coord':
+                        if not args[i+1].isdigit():
+                            broken = True
+                            response.append('7')
+                            break
+                        if int(args[i+1]) < 0 \
+                                or int(args[i+1]) >= self.field_size:
+                            broken = True
+                            response.append('8')
+                            break
+                        if not args[i+2].isdigit():
+                            broken = True
+                            response.append('9')
+                            break
+                        if int(args[i+2]) < 0 \
+                                or int(args[i+2]) >= self.field_size:
+                            broken = True
+                            response.append('10')
+                            break
+                        m_x = int(args[i+1])
+                        m_y = int(args[i+2])
+                    case _: continue
+
+        if not broken:
+            response.append('0')
+            response.append(monster["name"])
+            response.append(monster["hp"])
+            response.append(str(m_x))
+            response.append(str(m_y))
+            response.append(monster["message"])
+            
+            key = self.key((m_x, m_y))
+            if self.monsters.setdefault(key, None) != None:
+                del self.monsters[key]
+                response.append("Replaced the old monster")
+
+            self.monsters[key] = Monster(monster['name'], monster['hp'], monster['message'])
+
+        print("LOG: response", response)
+        return response
+
+    def attack(self, player, args):
+        response = []
+        position = player.position()
+        key = self.key(position)
+        args = shlex.split(args)
+        print("LOG: ", 'attack', args)
+
+        if self.monsters.setdefault(key, None) == None \
+        or self.monsters[key].name != args[0]:
+            response.append('1')
+            return response
+
+        name, dmg, hp = self.monsters[key].damage(int(args[1]))
+        response.append(str(name))
+        response.append('0')
+        response.append(str(dmg))
+        response.append(str(hp))
+        
+        if not hp:
+            del self.monsters[key]
+
+        print("LOG: response", response)
+        return response
+
+MUD_GAME = Game()
+users = dict()
+
+async def handler(reader, writer):
+    client = writer.get_extra_info("peername")
+    print(f'New Client on {client}')
+    my_id = None
+    my_player = None
+    my_queue = asyncio.Queue()
+    my_cmd = asyncio.create_task(reader.readline())
+    receive = asyncio.create_task(my_queue.get())
+
+    while not reader.at_eof():
+        done, pending = await asyncio.wait([my_cmd, receive], return_when=asyncio.FIRST_COMPLETED)
+
+        for request in done:
+            if request is my_cmd:
+                my_cmd = asyncio.create_task(reader.readline())
+                print(f'{client}: {request.result()}')
+                if (not request.result()):
+                    break;
+
+                cmd, *args = shlex.split(request.result().decode())
+                print("LOG: ", client, request.result().decode())
+                print("LOG: ", args)
+                answer = ""
+                match cmd:
+                    case 'register':
+                        if args[0] in users.keys():
+                            writer.write('LoginError: Exists user with this id\n'.encode())
+                            done = None
+                            break
+
+                        writer.write(f'0:You are logged in with  id {args[0]}\n'.encode())
+                        my_id = args[0]
+                        my_player = Player()
+                        users[my_id] = my_queue
+                        print(f"{client} logs in as {my_id}\n")
+
+                        for user in users.values():
+                            if user is not my_queue:
+                                await user.put(f'{my_id} join the MUD')                    
+                    
+
+                    case 'move':
+                        method, *args = args
+                        response = MUD_GAME.move(my_player, method, shlex.join(args))
+                        answer = move_answer(*response)
+                    case 'addmon':
+                        response = MUD_GAME.addmon(shlex.join(args))
+                        answer = addmon_answer(*response)
+
+                        for user in users.values():
+                            if user is not my_queue:
+                                await user.put(f'{my_id} {answer}')
+                    case 'attack':
+                        response = MUD_GAME.attack(my_player, shlex.join(args))
+                        answer = attack_answer(*response)
+
+                        for user in users.values():
+                            if user is not my_queue:
+                                await user.put(f'{my_id} {answer}')
+                    # case ['quit']:
+                    #     if not my_id:
+                    #         writer.write('NoAccessError: You are non-authorized client, please log in\n'.encode())
+                    #         break
+
+                    #     client = writer.get_extra_info("peername")
+                    #     print(f"{client} logs out from {my_id}\n")
+                    #     del users[my_id]
+                    #     my_id = None
+                    
+                    case _:
+                        continue
+                writer.write(answer.encode())
+                    
+
+            if request is receive:
+                receive = asyncio.create_task(my_queue.get())
+                writer.write(f"{request.result()}\n".encode())
+                await writer.drain()
+
+    for user in users.values():
+        if user is not my_queue:
+            await user.put(f'{my_id} left the MUD')
+
+    my_cmd.cancel()
+    receive.cancel()
+    if my_id:
+        del users[my_id]
+    writer.close()
+    await writer.wait_closed()
+    print(f'{client} left')
+
+async def main():
+    print('Start working')
+    server = await asyncio.start_server(handler, '0.0.0.0', 1337)
+    print('activate server')
+    async with server:
+        print('Server Forever')
+        await server.serve_forever()
+
+asyncio.run(main())
