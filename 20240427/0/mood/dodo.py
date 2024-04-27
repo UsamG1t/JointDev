3332a2f6e9ab1096b20c44369141f20e5e82aabc
Stephen <UsamUNX>
2024-04-27 13:24:06 +0300

    Initial

--- 20240427/0/mood/dodo.py
+++ 20240427/0/mood/dodo.py
@@ -1,10 +1,11 @@
 def task_html():
     """Generate HTML docs."""
     return {
-        'actions': ['sphinx-build -M html "." "_build"']
+        'actions': ['sphinx-build -M html "Documentation" "_build"'],
+        'file_dep': ["Documentation/index.rst", "Documentation/server_documentation.rst", "server/__init__.py"]
     }
 
-def task_erase()"
+def task_erase():
     """Erase all trash"""
     return {
         'actions': ['git clean -xdf']
