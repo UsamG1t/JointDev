4e72c5c8f4030abf09b4039679a3316882737c46
Stephen <UsamUNX>
2024-05-13 13:04:42 +0300

    Initial 20240513

---
+++ 20240513/0/mood/dodo.py
@@ -0,0 +1,54 @@
+
+from glob import iglob, glob
+from doit.tools import create_folder
+
+HTMLINDEX = "_build/html/index.html"
+
+def task_html():
+    """Generate HTML docs."""
+    return {
+        'actions': ['sphinx-build -M html "." "_build"'],
+        'file_dep': ["index.rst", "server_documentation.rst", "server/__init__.py", "po/ru_RU.UTF-8/LC_MESSAGES/MMUD_Locale.mo"],
+        'targets': [HTMLINDEX]
+    }
+
+def task_erase():
+    """Erase all trash"""
+    return {
+        'actions': ['git clean -xdf']
+    }
+
+
+def task_pot():
+    """Re-create .pot ."""
+    return {
+            'actions': ['pybabel extract -o MMUD.pot .'],
+            'file_dep': [*iglob('*.py')],
+            'targets': ['MMUD.pot'],
+           }
+
+def task_po():
+    """Update translations."""
+    return {
+            'actions': ['pybabel update -D MMUD_Locale -d po -l ru_RU.UTF-8 -i MMUD.pot'],
+            'file_dep': ['MMUD.pot'],
+            'targets': ['po/ru_RU.UTF-8/LC_MESSAGES/MMUD_Locale.po'],
+           }
+
+def task_il8n():
+    """Compile translations."""
+    return {
+            'actions': [
+                (create_folder, [f'po/ru_RU.UTF-8/LC_MESSAGES']),
+                f'pybabel compile -D MMUD_Locale -l ru_RU.UTF-8 -i po/ru_RU.UTF-8/LC_MESSAGES/MMUD_Locale.po -d po'
+                       ],
+            'file_dep': ['po/ru_RU.UTF-8/LC_MESSAGES/MMUD_Locale.po'],
+            'targets': ['po/ru_RU.UTF-8/LC_MESSAGES/MMUD_Locale.mo'],
+           }
+
+def task_test():
+    """Update translations."""
+    return {
+            'actions': ['python3.10 -m unittest test_client_server.py'],
+            'file_dep': ['po/ru_RU.UTF-8/LC_MESSAGES/MMUD_Locale.mo'],
+           }
